5ffc3f655d9d5c45988868a98de4719be07a2e78
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2019-11-18 11:48:54 +0800

    refactor some changes

--- simplewebapp/index.js
+++ simplewebapp/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const app = express();
 
+const PORT = 4000;
+
 app.get("/", (req, res) => {
-  res.send("Hello World Docker Nodejs Server Running on 8080");
+  res.send(`Hello World Docker Nodejs Server Running on ${PORT}`);
 });
 
-app.listen(8080, () => {
-  console.log("Listening on port 8080");
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
